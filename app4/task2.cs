c53dd05ffa6f94f88175f2db32e1734a891bf815
Abbas Azisov <abbastktl@gmail.com>
2020-01-14 22:28:51 +0600

    third commit

--- app4/task2.cs
+++ app4/task2.cs
@@ -9,37 +9,40 @@ namespace app4
         static void Main(string[] args)
         {
             int n = Convert.ToInt32((Console.ReadLine()));
-            int n1,n2,n3; int min=0; 
-            for(int i=0;i<n;i++)
+            int[] arr=new int[3]; int min=0; 
+            for(int j=0;j<n;j++)
             {
                 String oper = Console.ReadLine();
                 if(oper=="min")
                 {
-                    n1 = Convert.ToInt32(Console.ReadLine());
-                    n2 = Convert.ToInt32(Console.ReadLine());
-                    n3 = Convert.ToInt32(Console.ReadLine());
-                    if (n1 < n2 && n1 < n3 || n1==n2)
-                        min = n1;
-                    else if (n2 < n1 && n2 < n3 || n2==n3)
-                        min = n2;
-                    else if (n3 < n1 && n3 < n2 || n3==n1)
-                        min = n3;
+                    for(int i=0;i<3;i++)
+                    {
+                        arr[i]= Convert.ToInt32(Console.ReadLine());
+                    }                  
+                    if (arr[0] < arr[1] && arr[0] < arr[2] || arr[0]==arr[1])
+                        min = arr[0];
+                    else if (arr[1] < arr[0] && arr[1] < arr[2] || arr[1]==arr[2])
+                        min = arr[1];
+                    else if (arr[2] < arr[0] && arr[2] < arr[1] || arr[2]==arr[0])
+                        min = arr[2];
                     Console.WriteLine(min);
                 }
                 else if(oper=="mean")
                 {
-                    n1 = Convert.ToInt32(Console.ReadLine());
-                    n2 = Convert.ToInt32(Console.ReadLine());
-                    n3 = Convert.ToInt32(Console.ReadLine());
-                    int mean = (n1 + n2 + n3) / 3;
+                    for (int i = 0; i < 3; i++)
+                    {
+                        arr[i] = Convert.ToInt32(Console.ReadLine());
+                    }
+                    int mean = (arr[0] + arr[1] + arr[2]) / 3;
                     Console.WriteLine(mean);
                 }
                 else if(oper=="eye")
                 {
-                    n1 = Convert.ToInt32(Console.ReadLine());
-                    n2 = Convert.ToInt32(Console.ReadLine());
-                    n3 = Convert.ToInt32(Console.ReadLine());
-                    double p = 0.30*n1+n2*0.59+n3*0.11;
+                    for (int i = 0; i < 3; i++)
+                    {
+                        arr[i] = Convert.ToInt32(Console.ReadLine());
+                    }
+                    double p = 0.30*arr[0]+arr[1]*0.59+arr[2]*0.11;
                     int eye = Convert.ToInt32(p);
                     Console.WriteLine(eye);
                 }
