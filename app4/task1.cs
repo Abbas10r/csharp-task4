ec53e50a21b23a8c44e5e9d790961c3a671c1165
Abbas Azisov <abbastktl@gmail.com>
2020-01-14 21:44:47 +0600

    second commit

---
+++ app4/task1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace app4
+{
+    class task1
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            while (n != 0)
+            {
+                n = Convert.ToInt32(Console.ReadLine());
+                int[,] arr = new int[n, n];
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (n > 2 && i != 0 && j != 0 && i != n - 1 && j != n - 1)
+                        {
+                            arr[i, j] = 2;
+                        }
+                        else
+                        {
+                            arr[i, j] = 1;
+                        }
+                        Console.Write("  " + arr[i, j]);
+                    }
+                    Console.Write("\n");
+                }
+            }          
+        }
+    }
+}
---
+++ app4/task2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace app4
+{
+    class task2
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32((Console.ReadLine()));
+            int n1,n2,n3; int min=0; 
+            for(int i=0;i<n;i++)
+            {
+                String oper = Console.ReadLine();
+                if(oper=="min")
+                {
+                    n1 = Convert.ToInt32(Console.ReadLine());
+                    n2 = Convert.ToInt32(Console.ReadLine());
+                    n3 = Convert.ToInt32(Console.ReadLine());
+                    if (n1 < n2 && n1 < n3 || n1==n2)
+                        min = n1;
+                    else if (n2 < n1 && n2 < n3 || n2==n3)
+                        min = n2;
+                    else if (n3 < n1 && n3 < n2 || n3==n1)
+                        min = n3;
+                    Console.WriteLine(min);
+                }
+                else if(oper=="mean")
+                {
+                    n1 = Convert.ToInt32(Console.ReadLine());
+                    n2 = Convert.ToInt32(Console.ReadLine());
+                    n3 = Convert.ToInt32(Console.ReadLine());
+                    int mean = (n1 + n2 + n3) / 3;
+                    Console.WriteLine(mean);
+                }
+                else if(oper=="eye")
+                {
+                    n1 = Convert.ToInt32(Console.ReadLine());
+                    n2 = Convert.ToInt32(Console.ReadLine());
+                    n3 = Convert.ToInt32(Console.ReadLine());
+                    double p = 0.30*n1+n2*0.59+n3*0.11;
+                    int eye = Convert.ToInt32(p);
+                    Console.WriteLine(eye);
+                }
+            }          
+        }
+    }
+}
