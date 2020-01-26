8cb356a4db0591fc2ddcf247c67d3902eba16d77
Abbas Azisov <abbastktl@gmail.com>
2020-01-26 15:03:36 +0600

    did 1st task

--- app4/task1.cs
+++ app4/task1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using static System.Convert;
+using static System.Console;
 
 namespace app4
 {
@@ -6,28 +8,48 @@ namespace app4
     {
         static void Main(string[] args)
         {
-            int n = 1;
-            while (n != 0)
+            int a, b, c, d, e, f, g, h, i, j = 1, N, p, q, r=0;          
+            while (true)
             {
-                n = Convert.ToInt32(Console.ReadLine());
-                int[,] arr = new int[n, n];
-                for (int i = 0; i < n; i++)
+                N = ToInt32(ReadLine());
+                if (N == 0)
+                    break;
+                else
                 {
-                    for (int j = 0; j < n; j++)
+                    j = 1;p = 0;q = 0;
+                    int[,] arr = new int[N, N];
+                    i = N;
+                    if (N % 2 == 0)
+                        r = N / 2;
+                    else if (N % 2 == 1)
+                        r = (N / 2) + 1;
+                    for (c = 1; c <=r; c++)
                     {
-                        if (n > 2 && i != 0 && j != 0 && i != n - 1 && j != n - 1)
+                        for (a = p; a < i; a++)
                         {
-                            arr[i, j] = 2;
+                            for (b = q; b < i; b++)
+                                arr[a,b] = j;
                         }
-                        else
-                        {
-                            arr[i, j] = 1;
-                        }
-                        Console.Write("  " + arr[i, j]);
+                        j++;
+                        p++; 
+                        q++; 
+                        i--;
                     }
-                    Console.Write("\n");
+                    for (g = 0; g < N; g++)
+                    {
+                        for (h = 0; h < N; h++)
+                        {
+                            if (h == 0)
+                                Write($"   {arr[g, h]}");
+                            else
+                                Write($"    {arr[g, h]}");                              
+                        }
+                        WriteLine();
+                    }
+                    WriteLine();
                 }
-            }          
+                
+            }      
         }
     }
 }
